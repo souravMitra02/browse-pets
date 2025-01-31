77f54eea775e4cd9ceca2ff5a1eb77f6d9f94086
souravMitra02 <inf.souravmitra@gmail.com>
2025-01-31 20:46:13 +0600

    added to animition

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  theme: {
+    extend: {
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        float: "float 2s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-2deg)" },
+          "50%": { transform: "rotate(2deg)" },
+        },
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-5px)" },
+        },
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
